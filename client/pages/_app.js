bb450fe34d858da80ec0bb17333db2fac7f1518f
Alex-Maxx <alexn.serban@gmail.com>
2021-04-01 19:50:03 +0300

    first commit

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+function GameHighlightsApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} 
+  />)
+}
+
+export default GameHighlightsApp
---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+
+      <Head>
+        <title>GameHighlights</title>
+      </Head>
+      
+      <h1>Welcome To Next</h1>
+    </div>
+  )
+}
+
+export default Home;
